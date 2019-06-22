d7715cc49be2799485958d87dce5cb8267d6917d
karlos <daleytd@hotmail.com>
2019-06-22 00:02:50 -0500

    [HT-2]. Modify Class appconfig in methodc datasourceMyhsql.

--- src/main/java/com/nucleoti/alpes/ti/config/AppConfig.java
+++ src/main/java/com/nucleoti/alpes/ti/config/AppConfig.java
@@ -29,15 +29,15 @@ public class AppConfig {
         driverManagerDataSource.setPassword("admin");
         return driverManagerDataSource;
     }*/
-//    @Bean(name = "dataSource")
-//    public DriverManagerDataSource dataSource() {
-//        DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
-//        driverManagerDataSource.setDriverClassName("com.mysql.jdbc.Driver");
-//        driverManagerDataSource.setUrl("jdbc:mysql://localhost:3306/bdPrueba");
-//        driverManagerDataSource.setUsername("root");
-//        driverManagerDataSource.setPassword("");
-//        return driverManagerDataSource;
-//    }
+    @Bean(name = "dataSource")
+    public DriverManagerDataSource dataSourceMysql() {
+        DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
+        driverManagerDataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        driverManagerDataSource.setUrl("jdbc:mysql://localhost:3306/bdPrueba");
+        driverManagerDataSource.setUsername("root");
+        driverManagerDataSource.setPassword("");
+        return driverManagerDataSource;
+    }
 
 
 
