b45f187463e0a9824c7334b8499da78745ff1f5c
orlandlope <orlandlope@gmail.com>
2019-08-15 19:40:00 -0500

    add Comment

--- src/main/java/com/nucleoti/alpes/ti/config/SpringBootAlpesApplication.java
+++ src/main/java/com/nucleoti/alpes/ti/config/SpringBootAlpesApplication.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SpringBootAlpesApplication implements WebMvcConfigurer {
-
+	//Modicaciones II
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootAlpesApplication.class, args);
 	}
